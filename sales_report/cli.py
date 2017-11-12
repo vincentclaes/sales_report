2317ee2ef39bf25cbb69efaa115592328a7e13e8
vincent.claes <vincent.claes@porphyrio.com>
2017-11-12 09:46:31 +0100

    add function for forecasting

--- sales_report/cli.py
+++ sales_report/cli.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import logging
 
 from sales_report import ROOT
 from sales_report.monthly import MonthlyReport
@@ -21,19 +22,29 @@ def monthly(args):
     generator.create_html_report(template_vars, args.template, args.path)
 
 
+def forecast(args):
+    # todo - use a non linear model to forecast sales.
+    logging.info('forecast needs to be implemented')
+
+
 def get_parser():
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(help='sub-command help')
+
     ht = "create a report"
-    parser_description = subparsers.add_parser('monthly', help=ht)
-    parser_description.add_argument('--month', type=int, default=-1,
-                                    help="define how many months ago you want to check")
-    parser_description.add_argument('--dataset', type=str, default=default_dataset,
-                                    help="the path to the dataset")
-    parser_description.add_argument('--path', type=str, default=default_output,
-                                    help="the output path of the report.")
-    parser_description.add_argument('--template', type=str, default=default_template,
-                                    help="the path of the template")
-    parser_description.set_defaults(func=monthly)
+    parser_monthly = subparsers.add_parser('monthly', help=ht)
+    parser_monthly.add_argument('--month', type=int, default=-1,
+                                help="define how many months ago you want to check")
+    parser_monthly.add_argument('--dataset', type=str, default=default_dataset,
+                                help="the path to the dataset")
+    parser_monthly.add_argument('--path', type=str, default=default_output,
+                                help="the output path of the report.")
+    parser_monthly.add_argument('--template', type=str, default=default_template,
+                                help="the path of the template")
+    parser_monthly.set_defaults(func=monthly)
+
+    ht = "forecast sales"
+    parser_monthly = subparsers.add_parser('forecast', help=ht)
+    parser_monthly.set_defaults(func=forecast)
 
     return parser
